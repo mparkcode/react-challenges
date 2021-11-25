0c7298c6e1259d0baba265c7f8ecbe6db57809e8
Michael Park <mparkcode@gmail.com>
2021-11-25 23:14:12 +0000

    import css

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import css from './css/Sidebar.module.css'
 
 const Sidebar = () => {
     return (
-        <div>
+        <div className={css.sidebar}>
             <a href="#">My photos</a>
             <a href="#">My illustrations</a>
             <a href="#">My Paintings</a>

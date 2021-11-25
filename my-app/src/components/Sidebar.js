a9aad57a3ffd5bb031c217de9efa95b3d4ad575a
Michael Park <mparkcode@gmail.com>
2021-11-25 23:11:29 +0000

    challenge 01

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Sidebar = () => {
+    return (
+        <div>
+            <a href="#">My photos</a>
+            <a href="#">My illustrations</a>
+            <a href="#">My Paintings</a>
+        </div>
+    )
+}
+
+export default Sidebar
